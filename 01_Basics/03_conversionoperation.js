0364e5a55f7bd5ead7ceafbee81c83cbe56105f3
amankumardas2608 <kumardasaman1010@gmail.com>
2026-06-27 18:42:39 +0000

    conversion of datatypes discussion

---
+++ 01_Basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score = "Aman";
+console.log(typeof score);
+
+let valueinnumber = Number(score);
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber); //output NaN => not a number..
+
+//----CONCLUSION----
+// "33" => 33
+// "33abc" => NaN
+// true => 1 / false => 0;
+
+let isloggedin = "A";
+let booleanisloggedin = Boolean(isloggedin);
+console.log(typeof booleanisloggedin);
+// console.log(booleanisloggedin);
+
+//----CONCLUSION----
+// 1 => true / 0 => false
+// "" => false
+// "Aman" => true
+
+let somenumber = 10;
+let stringnumber = String(somenumber);
+console.log(typeof stringnumber);
+console.log(stringnumber);
