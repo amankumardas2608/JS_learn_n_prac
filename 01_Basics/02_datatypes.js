0b19cd3e9f962425228add868e8c59813fab1fdd
amankumardas2608 <kumardasaman1010@gmail.com>
2026-06-27 17:32:24 +0000

    datatypes_discusion

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js code as newer version
+
+// alert(5+5) //we are using nodejs... not browser
+
+console.log(10+10);
+console.log("AKD....");
+// console.log(10
+//     +
+//     30
+// ); //code rediability should be high...
+
+let name = "Aman";
+let age = 22;
+let isvalid = true;
+
+//Primitive datatypes are given below....
+
+// number => 2 to the power 53
+// BigInt =>
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique    
+
+//Non-Primitive datatypes are given below....
+
+//object
+
+console.log(typeof "Aman");
+console.log(typeof age);
+console.log(typeof null);  //output type- object.
+console.log(typeof undefined);  //output type- undefined.
