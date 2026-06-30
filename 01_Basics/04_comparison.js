05279bb349a68c4e839e642486718a27e5eaa0b5
amankumardas2608 <kumardasaman1010@gmail.com>
2026-06-30 21:04:29 +0000

    comparison_operator_discussion

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,26 @@
+//basic comparison---------------------------------------------------------------------------
+// console.log(2 < 1);
+// console.log(2 > 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+//ans will come in true or false...
+
+//Jab datatypes alag alag ho toh then comparison kaise---------------------------------------
+// console.log("2" > 1); 
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+
+//line 14 to 20 - avoid writing this type of codes.. 
+// it may put you in confusion but basic you should know.
+
+console.log("2" == 2);
+console.log("2" === 2);
