e2cfe0ac5da5fe96f7d5dcd5e91896d5f714d549
amankumardas2608 <kumardasaman1010@gmail.com>
2026-07-30 01:01:30 +0000

    while and do while loop discussion.

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+//while loop
+
+// let i = 0
+
+// while (i<=10) {
+//     console.log(`value of index is ${i}`)
+//     i+=2
+// }
+
+// let myarr = ["flash", "batman", "superman"]
+// let i = 1;
+// while(i<=myarr.length){
+//     console.log(`Name of ${i} superhero is ${myarr[i]}`)
+//     i++
+// }
+
+//do-while loop............................
+
+let score = 45628
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score <= 10);
