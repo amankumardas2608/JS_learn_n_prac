c051f81ad7f43d811b519fecc27e48f7299afd68
amankumardas2608 <kumardasaman1010@gmail.com>
2026-07-31 21:58:00 +0000

    for-of discussion.

---
+++ 05_iterations/three.js
@@ -0,0 +1,33 @@
+//for of
+
+// ["", "", ""]
+// [{}, {}, {}
+
+// const arr = [1, 2, 3, 4, 5]
+// for (const i of arr) {
+//     console.log(i)
+// }
+// const greetings = "hello world!"
+// for(const i in greetings){
+//     console.log(`Each character is ${greetings[i]}`);
+// }
+
+//Maps........
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+// // console.log(map)
+// for(const [key, value] of map){
+//     console.log(key, '->', value)
+// }
+
+const myobj = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+// }
+// for(const [key, value] of myobj){
+//     console.log(key, '->', value)
+// }
+
