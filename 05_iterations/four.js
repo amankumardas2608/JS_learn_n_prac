b0be8183714fbcfd194a26d618d75d420f3a1877
amankumardas2608 <kumardasaman1010@gmail.com>
2026-07-31 21:59:05 +0000

    for-in discussion.

---
+++ 05_iterations/four.js
@@ -0,0 +1,32 @@
+//for in loop....
+const myobj = {
+    js: "javascript", 
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+// for (const key in myobj) {
+//      console.log(key)
+// }
+// for (const key in myobj) {
+//      console.log(myobj[key])
+// }
+// for (const key in myobj) {
+//      console.log(`${key} represents ${myobj[key]}.`)
+// }
+
+
+// const prog = ["js", "rb", "py", "java", "cpp"]
+// for (const key in prog) {
+//     console.log(key) //arr keys by default number..
+//     console.log(prog[key]) //values
+// }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+// for (const key in map) {
+//     console.log(key, '->', map[key]) //nothing prints...so it is not iterable..
+// }
+
